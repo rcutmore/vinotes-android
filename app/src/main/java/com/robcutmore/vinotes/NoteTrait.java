440845b071c74e90ee4fc7d738480df605bb94ee
Rob Cutmore <robcutmore@gmail.com>
2015-05-30 10:50:35 -0400

    Add initial NoteTrait class.

---
+++ app/src/main/java/com/robcutmore/vinotes/NoteTrait.java
@@ -0,0 +1,24 @@
+package com.robcutmore.vinotes;
+
+
+public class NoteTrait {
+
+    private String url;
+    private String name;
+
+    public NoteTrait(final String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(final String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
