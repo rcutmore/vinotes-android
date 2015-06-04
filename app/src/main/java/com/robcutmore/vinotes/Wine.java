57dd6be6d5040557119642f740cd425dc5de5fbc
Rob Cutmore <robcutmore@gmail.com>
2015-06-04 19:12:43 -0400

    Update Wine to allow ID to be set only once.

--- app/src/main/java/com/robcutmore/vinotes/Wine.java
+++ app/src/main/java/com/robcutmore/vinotes/Wine.java
@@ -3,22 +3,25 @@ package com.robcutmore.vinotes;
 
 public class Wine {
 
-    private long id;
+    private Long id = null;
     private Winery winery;
     private String name;
     private int vintage;
 
-    public Wine(final long id, final Winery winery, final String name, final int vintage) {
-        this.id = id;
+    public Wine(final Winery winery, final String name, final int vintage) {
         this.winery = winery;
+        this.winery.addWine(this);
         this.name = name;
         this.vintage = vintage;
-
-        this.winery.addWine(this);
     }
 
     public long getId() {
         return this.id;
+    }
+
+    public void setId(final long id) {
+        // Only allow id to be set once.
+        this.id = this.id == null ? id : this.id;
     }
 
     public Winery getWinery() {
@@ -37,4 +40,5 @@ public class Wine {
     public String toString() {
         return String.format("%s %s %d", this.winery.toString(), this.name, this.vintage);
     }
+
 }
