31d9df4fb35c9c0be4d717efcb916c19e8425b69
Rob Cutmore <robcutmore@gmail.com>
2015-06-06 13:26:30 -0400

    Add initial base database helper class.

---
+++ app/src/main/java/com/robcutmore/vinotes/DatabaseHelper.java
@@ -0,0 +1,47 @@
+package com.robcutmore.vinotes;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.HashMap;
+
+
+abstract class DatabaseHelper extends SQLiteOpenHelper {
+
+    protected static final String DATABASE_NAME = "vinotes.db";
+    protected static final int DATABASE_VERSION = 1;
+    private String tableName = "";
+    private HashMap<String, String> columns = new HashMap<>();
+    private String sqlCreateTable = "";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(this.sqlCreateTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        String logMessage = String.format(
+            "Upgrading database from version %d to %d.", oldVersion, newVersion);
+        Log.w(DatabaseHelper.class.getName(), logMessage);
+
+        String sql = String.format("DROP TABLE IF EXISTS %s;", this.tableName);
+        database.execSQL(sql);
+        this.onCreate(database);
+    }
+
+    public HashMap<String, String> getColumns() {
+        return this.columns;
+    }
+
+    public String getTableName() {
+        return this.tableName;
+    }
+
+}
