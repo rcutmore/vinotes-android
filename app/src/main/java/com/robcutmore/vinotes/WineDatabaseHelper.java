4cdf64dc0772fc9161d5d7b200c0a89dea365b5c
Rob Cutmore <robcutmore@gmail.com>
2015-06-06 15:40:16 -0400

    Add initial wine database helper class.

---
+++ app/src/main/java/com/robcutmore/vinotes/WineDatabaseHelper.java
@@ -0,0 +1,32 @@
+package com.robcutmore.vinotes;
+
+import android.content.Context;
+
+import java.util.HashMap;
+
+
+public class WineDatabaseHelper extends DatabaseHelper {
+
+    private String tableName = "wines";
+    private HashMap<String, String> columns = new HashMap<>();
+    private String sqlCreateTable;
+
+    public WineDatabaseHelper(Context context) {
+        super(context);
+
+        // Store all columns in wines table.
+        this.columns.put("id", "_id");
+        this.columns.put("winery", "winery_id");
+        this.columns.put("name", "name");
+        this.columns.put("vintage", "vintage");
+
+        // Store SQL command to create wines table.
+        this.sqlCreateTable = String.format(
+            "CREATE TABLE %s(%s INTEGER PRIMARY KEY, %s INTEGER NOT NULL, " +
+            "%s TEXT NOT NULL, %s INTEGER NOT NULL);",
+            this.tableName, this.columns.get("id"), this.columns.get("winery"),
+            this.columns.get("name"), this.columns.get("vintage")
+        );
+    }
+
+}
