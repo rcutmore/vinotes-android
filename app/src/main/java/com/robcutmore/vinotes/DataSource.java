61df193b0eff599ce68e1ae18991aac691009ae7
Rob Cutmore <robcutmore@gmail.com>
2015-06-08 19:48:45 -0400

    Add initial base data source class.

---
+++ app/src/main/java/com/robcutmore/vinotes/DataSource.java
@@ -0,0 +1,24 @@
+package com.robcutmore.vinotes;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+
+abstract class DataSource {
+
+    protected SQLiteDatabase database;
+    protected DatabaseHelper dbHelper;
+    protected HashMap<String, String> dbColumns;
+
+    public void open() throws SQLException {
+        this.database = this.dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        this.dbHelper.close();
+    }
+
+}
