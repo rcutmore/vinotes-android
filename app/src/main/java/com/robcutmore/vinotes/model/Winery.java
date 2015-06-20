ff728d8953853d7e762155d3c34be1de506889e2
Rob Cutmore <robcutmore@gmail.com>
2015-06-20 14:44:02 -0400

    Remove wines variable from Winery class.

--- app/src/main/java/com/robcutmore/vinotes/model/Winery.java
+++ app/src/main/java/com/robcutmore/vinotes/model/Winery.java
@@ -1,13 +1,10 @@
 package com.robcutmore.vinotes.model;
 
-import java.util.HashMap;
-
 
 public class Winery {
 
     private Long id;
     private String name;
-    private HashMap<Long, Wine> wines = new HashMap<>();
 
     public Winery(final String name) {
         this.name = name;
@@ -30,27 +27,6 @@ public class Winery {
 
     public String getName() {
         return this.name;
-    }
-
-    public HashMap<Long, Wine> getWines() {
-        return this.wines;
-    }
-
-    public void setWines(final HashMap<Long, Wine> wines) {
-        this.wines = wines;
-    }
-
-    public void addWine(final Wine wine) {
-        // Add new wine only if it's not already in wines.
-        Wine existingWine = this.wines.get(wine.getId());
-        boolean isFound = existingWine != null;
-
-        // Make sure wine has an id set.
-        boolean hasId = wine.getId() != null;
-
-        if (!isFound && hasId) {
-            this.wines.put(wine.getId(), wine);
-        }
     }
 
     @Override
