9b7fd4ff163629cf673c13a18bb03d8411851a7e
Rob Cutmore <robcutmore@gmail.com>
2015-07-01 07:06:43 -0400

    Add initial WineRequest class.

---
+++ app/src/main/java/com/robcutmore/vinotes/request/WineRequest.java
@@ -0,0 +1,43 @@
+package com.robcutmore.vinotes.request;
+
+
+import com.robcutmore.vinotes.model.Wine;
+import com.robcutmore.vinotes.model.Winery;
+
+
+public class WineRequest {
+
+    public static Wine get(final long id) {
+        String response = sendGET(id);
+        Wine[] wines = parseResponse(response);
+        return (wines.length > 0) ? wines[0] : null;
+    }
+
+    public static Wine[] getAll() {
+        String response = sendGET();
+        Wine[] wines = parseResponse(response);
+        return wines;
+    }
+
+    private static String sendGET(final Long id) {
+        // Test stub, replace with request to API.
+        return "";
+    }
+
+    private static String sendGET() {
+        return sendGET(null);
+    }
+
+    private static Wine[] parseResponse(final String response) {
+        // Test stub, replace with code to parse JSON response.
+        Winery firstWinery = new Winery(1, "Test Winery 1");
+        Winery secondWinery = new Winery(2, "Test Winery 2");
+        Wine[] wines = new Wine[] {
+            new Wine(1, firstWinery, "Test Wine 1", 2011),
+            new Wine(2, firstWinery, "Test Wine 2", 2012),
+            new Wine(3, secondWinery, "Test Wine 3", 2014)
+        };
+        return wines;
+    }
+
+}
