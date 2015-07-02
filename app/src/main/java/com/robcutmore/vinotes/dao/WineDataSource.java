b94bceab778ffa922ab702e14213c38294821f86
Rob Cutmore <robcutmore@gmail.com>
2015-07-02 07:13:23 -0400

    Update method names in WineDataSource.

--- app/src/main/java/com/robcutmore/vinotes/dao/WineDataSource.java
+++ app/src/main/java/com/robcutmore/vinotes/dao/WineDataSource.java
@@ -22,46 +22,13 @@ public class WineDataSource extends DataSource {
         this.wineryDataSource = new WineryDataSource(context);
     }
 
-    public Wine createWine(final long id, final long wineryId, final String name, final int vintage) {
-        // Prepare wine values to be inserted into database.
-        ContentValues values = new ContentValues();
-        values.put(this.dbColumns.get("id"), id);
-        values.put(this.dbColumns.get("winery"), wineryId);
-        values.put(this.dbColumns.get("name"), name);
-        values.put(this.dbColumns.get("vintage"), vintage);
-
-        // Insert wine into database if it doesn't exist yet.
-        String table = this.dbHelper.getTableName();
-        this.database.insertWithOnConflict(table, null, values, SQLiteDatabase.CONFLICT_IGNORE);
-
-        // Create and return wine with given information.
-        Winery winery = this.wineryDataSource.get(wineryId);
-        Wine wine = new Wine(winery, name, vintage);
-        wine.setId(id);
-        return wine;
-    }
-
-    public void deleteWine(final long id) {
+    public void remove(final long id) {
         String table = this.dbHelper.getTableName();
         String whereClause = String.format("%s = %d", this.dbColumns.get("id"), id);
         this.database.delete(table, whereClause, null);
     }
 
-    public Wine getWine(final long id) {
-        // Query wines table for wine with given id.
-        String table = this.dbHelper.getTableName();
-        String[] columns = this.getDatabaseTableColumns();
-        String whereClause = String.format("%s = %d", this.dbColumns.get("id"), id);
-        Cursor cursor = this.database.query(table, columns, whereClause, null, null, null, null);
-
-        // Store and return wine.
-        cursor.moveToFirst();
-        Wine wine = !cursor.isAfterLast() ? this.cursorToWine(cursor) : null;
-        cursor.close();
-        return wine;
-    }
-
-    public HashMap<Long, Wine> getAllWines() {
+    public HashMap<Long, Wine> getAll() {
         // Query wines table for all wines.
         String table = this.dbHelper.getTableName();
         String[] columns = this.getDatabaseTableColumns();
@@ -88,6 +55,33 @@ public class WineDataSource extends DataSource {
             this.dbColumns.get("vintage")
         };
         return columns;
+    }
+
+    private void addToDatabase(final Wine wine) {
+        // Prepare wine values to be inserted into database.
+        ContentValues values = new ContentValues();
+        values.put(this.dbColumns.get("id"), wine.getId());
+        values.put(this.dbColumns.get("winery"), wine.getWinery().getId());
+        values.put(this.dbColumns.get("name"), wine.getName());
+        values.put(this.dbColumns.get("vintage"), wine.getVintage());
+
+        // Insert wine into database if it doesn't exist yet.
+        String table = this.dbHelper.getTableName();
+        this.database.insertWithOnConflict(table, null, values, SQLiteDatabase.CONFLICT_IGNORE);
+    }
+
+    private Wine getFromDatabase(final long id) {
+        // Query wines table for wine with given id.
+        String table = this.dbHelper.getTableName();
+        String[] columns = this.getDatabaseTableColumns();
+        String whereClause = String.format("%s = %d", this.dbColumns.get("id"), id);
+        Cursor cursor = this.database.query(table, columns, whereClause, null, null, null, null);
+
+        // Store and return wine.
+        cursor.moveToFirst();
+        Wine wine = !cursor.isAfterLast() ? this.cursorToWine(cursor) : null;
+        cursor.close();
+        return wine;
     }
 
     private Wine cursorToWine(final Cursor cursor) {
