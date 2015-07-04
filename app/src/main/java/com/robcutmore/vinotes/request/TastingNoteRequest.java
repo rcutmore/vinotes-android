123cbbeabbd187d42f98db61466d461990b343eb
Rob Cutmore <robcutmore@gmail.com>
2015-07-04 08:40:34 -0400

    Add initial TastingNoteRequest class.

---
+++ app/src/main/java/com/robcutmore/vinotes/request/TastingNoteRequest.java
@@ -0,0 +1,44 @@
+package com.robcutmore.vinotes.request;
+
+
+import com.robcutmore.vinotes.model.TastingNote;
+import com.robcutmore.vinotes.model.Wine;
+import com.robcutmore.vinotes.model.Winery;
+
+
+public class TastingNoteRequest {
+
+    public static TastingNote get(final long id) {
+        String response = sendGET(id);
+        TastingNote[] notes = parseResponse(response);
+        return (notes.length > 0) ? notes[0] : null;
+    }
+
+    public static TastingNote[] getAll() {
+        String response = sendGET();
+        TastingNote[] notes = parseResponse(response);
+        return notes;
+    }
+
+    private static String sendGET(final Long id) {
+        // Test stub, replace with request to API.
+        return "";
+    }
+
+    private static String sendGET() {
+        return sendGET(null);
+    }
+
+    private static TastingNote[] parseResponse(final String response) {
+        // Test stub, replace with code to parse JSON request.
+        Winery winery = new Winery(1, "Test Winery");
+        Wine firstWine = new Wine(1, winery, "Test 1", 2012);
+        Wine secondWine = new Wine(2, winery, "Test 2", 2013);
+        TastingNote[] notes = {
+            new TastingNote(1, firstWine),
+            new TastingNote(2, secondWine)
+        };
+        return notes;
+    }
+
+}
