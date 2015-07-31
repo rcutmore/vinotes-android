e7164b47de519f75ca68330dd1893eac79ce6565
Rob Cutmore <robcutmore@gmail.com>
2015-07-31 07:34:59 -0400

    Update SelectWineryActivity to add winery search
    Added a list view containing various wineries stored in database to
    SelectWineryActivity. Also updated the activity so the winery list view is
    filtered as a user searches for a winery.

--- app/src/main/java/com/robcutmore/vinotes/ui/SelectWineryActivity.java
+++ app/src/main/java/com/robcutmore/vinotes/ui/SelectWineryActivity.java
@@ -3,18 +3,64 @@ package com.robcutmore.vinotes.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.robcutmore.vinotes.R;
+import com.robcutmore.vinotes.dao.WineryDataSource;
+import com.robcutmore.vinotes.model.Winery;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 
 public class SelectWineryActivity extends ActionBarActivity {
+
+    private EditText etWinerySearch;
+    private ListView lvWineries;
+
+    private WineryDataSource wineryDataSource;
+    private ArrayList<Winery> wineries;
+    private ArrayAdapter<Winery> wineriesAdapter;
+
+    // This will watch for any typing in etWinerySearch and filter lvWineries.
+    private final TextWatcher searchWatcher = new TextWatcher() {
+        @Override
+        public void afterTextChanged(Editable s) {}
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // Filter list view.
+            wineriesAdapter.getFilter().filter(s);
+        }
+    };
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_winery);
+
+        this.initializeDataSources();
+
+        this.etWinerySearch = (EditText) findViewById(R.id.etWinerySearch);
+        this.etWinerySearch.addTextChangedListener(this.searchWatcher);
+        this.lvWineries = (ListView) findViewById(R.id.lvWineries);
+
+        this.wineries = this.wineryDataSource.getAll(false);
+        this.wineriesAdapter = new ArrayAdapter<>(
+            this,
+            android.R.layout.simple_list_item_1,
+            this.wineries
+        );
+        this.lvWineries.setAdapter(this.wineriesAdapter);
     }
 
     @Override
@@ -22,6 +68,12 @@ public class SelectWineryActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_add_note, menu);
         return true;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        this.etWinerySearch.removeTextChangedListener(this.searchWatcher);
     }
 
     @Override
@@ -37,6 +89,15 @@ public class SelectWineryActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeDataSources() {
+        this.wineryDataSource = new WineryDataSource(this.getApplicationContext());
+        try {
+            this.wineryDataSource.open();
+        } catch (SQLException e) {
+            // handle error
+        }
     }
 
 }
